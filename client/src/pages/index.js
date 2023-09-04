e7a9571e6f13073365211cec3b0f23f205bf0e09
osiyeon <osiyun0313@gmail.com>
2023-09-04 20:55:02 +0900

    fix(oauth): kakao oauth 연동하기

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,50 +1,53 @@
-import { useRouter } from 'next/router';
-import { Button } from 'reactstrap';
-import React from 'react';
-import axios from 'axios'
+import { useRouter } from "next/router"
+import { Button } from "reactstrap"
+import React from "react"
+import axios from "axios"
+
+const REDIRECT_URI = "https://localhost:3001/api/user/test"
 
 const Index = () => {
-    const router = useRouter();
+  const router = useRouter()
 
-    const backgroundStyle = {
-        background: 'url(/images/first_page.svg)',
-        width: '100%',
-        height: '100vh',
-        ['background-size']: 'cover',
-        display: 'flex',
-        ['align-items']: 'center',
-        ['justify-content']: 'center'
-    };
-    
-    const buttonStyle = {
-        background: '#FAE100',
-        color: '#000000',
-        border: 'none',
-        left: '1.125rem',
-        width: '22.125rem',
-        height: '50px',
-        ['border-radius']: '1.875rem',
-        ['font-weight']: '600',
-        ['font-size']: '1rem',
-        ['margin-top']: '8.5rem'
-    };
+  const backgroundStyle = {
+    background: "url(/images/first_page.svg)",
+    width: "100%",
+    height: "100vh",
+    ["background-size"]: "cover",
+    display: "flex",
+    ["align-items"]: "center",
+    ["justify-content"]: "center",
+  }
 
-    return (
-        <div style={backgroundStyle}>
-            <Button
-                style={buttonStyle}
-                onClick={() => {
-                    axios.get('https://matching-appointment-server.run.goorm.io/api/user/kakao').then((res) => {
-                        console.log({res})
-                    })
-                    // router.push('/mainList');
-                }}
-            >
-                <img src="/images/kakao_icon.svg" />
-                카카오톡으로 3초만에 시작하기
-            </Button>
-        </div>
-    );
-};
+  const buttonStyle = {
+    background: "#FAE100",
+    color: "#000000",
+    border: "none",
+    left: "1.125rem",
+    width: "22.125rem",
+    height: "50px",
+    ["border-radius"]: "1.875rem",
+    ["font-weight"]: "600",
+    ["font-size"]: "1rem",
+    ["margin-top"]: "8.5rem",
+  }
 
-export default Index;
+  return (
+    <div style={backgroundStyle}>
+      {/* <img src="/images/kakao_login_medium_wide.png" /> */}
+      <Button
+        style={buttonStyle}
+        onClick={() => {
+          axios.get(`${REDIRECT_URI}`).then(res => {
+            console.log({ res })
+          })
+          // router.push("/mainList")
+        }}
+      >
+        <img src="/images/kakao_icon.svg" />
+        카카오톡으로 3초만에 시작하기
+      </Button>
+    </div>
+  )
+}
+
+export default Index
